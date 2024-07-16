21ada0bf3de31ca2ff5a3cd24f4875dee9e69c94
Sonam <sonam.panjwani29@gmail.com>
2024-07-16 15:48:08 +0530

    whatsapp

--- src/services/Whatapp.tsx
+++ src/services/Whatapp.tsx
@@ -3,10 +3,11 @@ import whatsapp from "../assets/whatsapp.png";
 function Whatapp() {
   return (
     <>
-      <div className="fixed bottom-12 right-4 z-50">
+      <div className="fixed bottom-10 right-4 z-50">
         <a
           href="https://wa.me/919009776655?text= Hello I would like to order a pizza?"
           target="_blank"
+          className="hover:opacity-75 transition-opacity duration-300"
         >
           <img src={whatsapp} alt="WhatsApp" className="w-20 h-20" />
         </a>
