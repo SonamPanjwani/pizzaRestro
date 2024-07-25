f68c2674fb84ef7dd5a419b44e3bf2962b014e90
Sonam <sonam.panjwani29@gmail.com>
2024-07-25 14:55:12 +0530

    update

--- src/features/order/ThankYou.tsx
+++ src/features/order/ThankYou.tsx
@@ -1,7 +1,10 @@
+import LinkStyle from "../../uiComponents/LinkStyle";
+
 function ThankYou() {
   return (
-    <div className="flex flex-col items-center bg-yellow-200 bg-opacity-30 text-stone-900 text-xl mt-5 p-4 font-semibold rounded-lg">
+    <div className="flex flex-col items-center gap-3 bg-yellow-200 bg-opacity-30 text-stone-900 text-xl mt-5 p-4 font-semibold rounded-lg">
       <h1> ThankYou for Visiting Fast React Pizza 😊</h1>
+      <LinkStyle to={"/"}>&larr; Home</LinkStyle>
     </div>
   );
 }
